2c7f0f0f09cb177ec1e183b3b80463d3b619e991
vrishketumishra <mishrarishi2006@gmail.com>
2026-03-23 01:29:01 +0530

    continue tomorrow

--- data/products.js
+++ data/products.js
@@ -15,7 +15,7 @@ export const products = [
     ]
   },
   {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    id: "f-327a-4ec4-896f-486349e85a3d15b6fc6",
     image: "images/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
