24dcab0ff28869753b7a10db497e4a063e7ad471
vrishketumishra <mishrarishi2006@gmail.com>
2026-03-24 21:08:32 +0530

    generate html for order summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,7 @@
 import {cart} from '../data/cart.js';
 import {products} from '../data/products.js';
+let cartSummaryHTML ='';
+
 
 cart.forEach((cartItem)=>{
  const productID=cartItem.productID;
@@ -8,8 +10,8 @@ cart.forEach((cartItem)=>{
     if(product.id===productID){
         matchingProduct=product;
     }
- })
-
+ });
+ cartSummaryHTML+=
 ` <div class="cart-item-container">
     <div class="delivery-date">
         Delivery date: Tuesday, June 21
@@ -17,18 +19,18 @@ cart.forEach((cartItem)=>{
 
     <div class="cart-item-details-grid">
         <img class="product-image"
-        src="images/products/athletic-cotton-socks-6-pairs.jpg">
+        src="${matchingProduct.image}">
 
         <div class="cart-item-details">
         <div class="product-name">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+           ${matchingProduct.name}
         </div>
         <div class="product-price">
-            $10.90
+            ${matchingProduct.priceCents/100}
         </div>
         <div class="product-quantity">
             <span>
-            Quantity: <span class="quantity-label">2</span>
+            Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary">
             Update
@@ -85,5 +87,9 @@ cart.forEach((cartItem)=>{
         </div>
     </div>
     </div>
-`
-})
+`;
+});
+
+document.querySelector('.js-order-summary')
+.innerHTML= cartSummaryHTML
+console.log(cartSummaryHTML);
