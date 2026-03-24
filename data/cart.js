7d2b5651f5e6675eba257f528a70578ed69c4624
vrishketumishra <mishrarishi2006@gmail.com>
2026-03-24 23:03:21 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,22 @@
-export let cart=[{
+export let cart=JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+cart=[{
     productID: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
 , quantity: 2,
 },{
     productID:'f-327a-4ec4-896f-486349e85a3d15b6fc6',
     quantity:1,
 }];
+}
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addtoCart(productID){
          let matchingItem;
+
            cart.forEach((cartItem)=>{
             if(productID===cartItem.productID){
                 matchingItem=cartItem;
@@ -22,6 +31,7 @@ export function addtoCart(productID){
             quantity:1
            });
            }
+           saveToStorage();
     }  
 export  function removeFromCart(productId){
         const newCart=[];
@@ -31,4 +41,5 @@ export  function removeFromCart(productId){
             }
         });
         cart= newCart;
+        saveToStorage();
     } 
